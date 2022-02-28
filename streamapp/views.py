384ad73bd14de9b7cc6afd6cc6976b85dafd574f
Dikshya-Kunwar <84702440+Dikshya-Kunwar@users.noreply.github.com>
2022-02-28 16:31:39 +0545

    for backup purposes

--- streamapp/views.py
+++ streamapp/views.py
@@ -15,9 +15,9 @@ def gen(camera):
 				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-def video_feed(request):
-	return StreamingHttpResponse(gen(VideoCamera()),
-					content_type='multipart/x-mixed-replace; boundary=frame')
+# def video_feed(request):
+# 	return StreamingHttpResponse(gen(VideoCamera()),
+# 					content_type='multipart/x-mixed-replace; boundary=frame')
 
 
 def webcam_feed(request):
@@ -29,6 +29,6 @@ def mask_feed(request):
 	return StreamingHttpResponse(gen(MaskDetect()),
 					content_type='multipart/x-mixed-replace; boundary=frame')
 					
-def livecam_feed(request):
-	return StreamingHttpResponse(gen(LiveWebCam()),
-					content_type='multipart/x-mixed-replace; boundary=frame')
+# def livecam_feed(request):
+# 	return StreamingHttpResponse(gen(LiveWebCam()),
+# 					content_type='multipart/x-mixed-replace; boundary=frame')
